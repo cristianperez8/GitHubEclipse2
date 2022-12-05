aa83f952b3176914bf00cb6227334ab12bca0ae0
Fernando Torres <amormur475@g.educaand.es>
2022-12-05 15:12:46 +0100

    ultimo acceso

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -3,6 +3,9 @@ public class EjemploGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hola DAM B");
+		String nombre = "Felix Guerra";
+		System.out.println(nombre);
 
 	}
 
