21195a0fdeca74e13dbcd950356f54f41c83837a
MJ valladolid <mvalcar1711@g.educaand.es>
2022-12-12 19:05:12 +0100

    version3

--- src/Ejemplogit/Prog1_0.java
+++ src/Ejemplogit/Prog1_0.java
@@ -5,15 +5,11 @@ import java.lang.Math;
 public class Prog1_0 {
      public static void main(String[] args) {
            // TODO Auto-generated method stub
-           Scanner leerradio = new Scanner(System.in);
-           int radio;
-           System.out.println("Introduce el radio de la esfera");
-           radio=leerradio.nextInt();
-           double area;
-           area = (Math.PI) * (Math.pow(radio, 2));
-           System.out.println("El área de la esfera es "+area);
-           System.out.println("Finalizado Version 2");
-     
+    	 double longitud;
+    	 longitud = 2 * (Math.PI)* radio;
+    	 System.out.println("La longitud de la circunferencia es "+longitud);
+    	 System.out.println("Finalizado Version 3");
+
      
      }
 }
