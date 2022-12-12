e0cf7a0f57e98c8b0fb1954df54df2d6d37ea8fa
JoseGalleg <119331098+JoseGalleg@users.noreply.github.com>
2022-12-12 18:54:18 +0100

    m,

--- src/prog1_0/Prog1_0.java
+++ src/prog1_0/Prog1_0.java
@@ -1,15 +1,9 @@
 package prog1_0;
 import java.util.Scanner;
-import java.lang.Math;
 public class Prog1_0 {
 public static void main(String[] args) {
-// TODO Auto-generated method stub
-Scanner leerradio = new Scanner(System.in);
-int radio;
-System.out.println("Introduce el radio de la esfera");
-radio=leerradio.nextInt();
- 
-double longitud;
-longitud = 2 * (Math.PI)* radio; 
+try (Scanner leerradio = new Scanner(System.in)) {
+	System.out.println("Introduce el radio de la esfera");
+} 
 }
 }
