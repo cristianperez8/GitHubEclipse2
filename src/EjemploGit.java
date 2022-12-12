437a89e5dafadfb4227e53c92c238e199fde3afa
Fernando Torres <amormur475@g.educaand.es>
2022-12-12 16:26:49 +0100

    Principal1.1

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -1,17 +1,21 @@
-
-import java.util.*;
+import java.util.Scanner;
 public class EjemploGit {
 	
-public static void main(String[] args) {
-// TODO Auto-generated method stub	
-Scanner sc = new Scanner(System.in);
-int numero1, numero2, resultado;
-System.out.print("Introduce primer número: ");
-numero1 = sc.nextInt();
-System.out.print("Introduce segundo número: ");
-numero2 = sc.nextInt();
-
-
-}
-}
-
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub 
+	Scanner sc = new Scanner(System.in);
+	int numero1, numero2, resultado;
+	System.out.print("Introduce primer número: ");
+	numero1 = sc.nextInt();
+	System.out.print("Introduce segundo número: ");
+	numero2 = sc.nextInt();
+	
+	resultado = suma(numero1, numero2);
+	System.out.println("Suma: " + resultado);
+	}
+	public static int suma(int a, int b) {
+		int c;
+		c = a + b;
+		return c;
+		} 
+	}
