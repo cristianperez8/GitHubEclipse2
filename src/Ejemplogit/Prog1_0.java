19022262376accf9e92b8e185bdbf332e5c3c506
MJ valladolid <mvalcar1711@g.educaand.es>
2022-12-12 19:14:07 +0100

    version4

--- src/Ejemplogit/Prog1_0.java
+++ src/Ejemplogit/Prog1_0.java
@@ -5,10 +5,8 @@ import java.lang.Math;
 public class Prog1_0 {
      public static void main(String[] args) {
            // TODO Auto-generated method stub
-    	 double longitud;
-    	 longitud = 2 * (Math.PI)* radio;
-    	 System.out.println("La longitud de la circunferencia es "+longitud);
-    	 System.out.println("Finalizado Version 3");
+    	 System.out.println("");
+    	 System.out.println("Version definitiva y refundida");
 
      
      }
