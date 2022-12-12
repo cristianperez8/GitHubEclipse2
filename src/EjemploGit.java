cf1f0e78061d31b0c2ce173614701015930931a8
Fernando Torres <amormur475@g.educaand.es>
2022-12-12 18:55:11 +0100

    Version1

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
+import java.util.Scanner;
+import java.lang.Math;
 public class EjemploGit {
 	
+	public class Prog1_0 {
 	public static void main(String[] args) {
-	// TODO Auto-generated method stub 
-	Scanner sc = new Scanner(System.in);
-	int numero1, numero2, resultado;
-	System.out.print("Introduce primer número: ");
-	numero1 = sc.nextInt();
-	System.out.print("Introduce segundo número: ");
-	numero2 = sc.nextInt();
-	
-	resultado = resta(numero1, numero2);
-	System.out.println("resta: " + resultado);
-	}
-	public static int resta(int a, int b) {
-		int c;
-		c = a - b;
-		return c;
+	// TODO Auto-generated method stub
+	Scanner leerradio = new Scanner(System.in);
+	int radio;
+	System.out.println("Introduce el radio de la esfera");
+	radio=leerradio.nextInt();
+	double volumen;
+	volumen = (4 * (Math.PI) * (Math.pow(radio,3))/3);
 		}
-
 	}
+	
+	}
+
